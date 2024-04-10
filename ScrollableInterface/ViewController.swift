bb50566ab219fe92e0c9fd274b581ef5f76f1cd9
sumanthpalliboina <palleboinasumanth@gmail.com>
2024-04-10 17:54:31 +0530

    scrolling the elements on interface

---
+++ ScrollableInterface/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollableInterface
+//
+//  Created by Palliboina on 10/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
