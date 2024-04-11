8b5a96e6bbe680b656a9929d36d41667b979d545
sumanthpalliboina <palleboinasumanth@gmail.com>
2024-04-11 20:09:55 +0530

    present modal segue from one scene to another as modal

---
+++ SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  ScrollableInterface
+//
+//  Created by Palliboina on 11/04/24.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
